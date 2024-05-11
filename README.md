3bd29ee530e442a2b0483a352440af5609ce3343
gaussguan <gaussguan@tencent.com>
2024-05-11 10:25:55 +0800

    feat: 27. 移除元素

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # leetgo
 leetcode for golang
+
+## 日拱一卒
+
+ - 2024年5月11日   
+ ```shell 
+ go test -v -run TestMerge
+ ```
+ ```shell
+ go test -v -run TestRemoveElement
+ ```
--- leetgo_test.go
+++ leetgo_test.go
@@ -5,6 +5,19 @@ import (
 	"testing"
 )
 
+// Test 27. 移除元素
+func TestRemoveElement(t *testing.T) {
+	nums := []int{3, 2, 2, 3}
+	val := 3
+	length := removeElement(nums, val)
+	assert.Equal(t, nums[:length], []int{2, 2})
+
+	nums = []int{0, 1, 2, 2, 3, 0, 4, 2}
+	val = 2
+	length = removeElement(nums, val)
+	assert.Equal(t, nums[:length], []int{0, 1, 3, 0, 4})
+}
+
 // Test 88. 合并两个有序数组
 func TestMerge(t *testing.T) {
 	nums1 := []int{1, 2, 3, 0, 0, 0}
